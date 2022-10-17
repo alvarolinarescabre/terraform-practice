ac55859274ea1c0e6a2a5b59d6380edda56bf622
Alvaro "Chamo" Linares Cabre <alinares@paradigmadigital.com>
2022-10-17 21:12:22 +0200

    Update provider.tf

--- Solution_EC2_Template/provider.tf
+++ Solution_EC2_Template/provider.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
+    
+    tls {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
   }
 }
 
 provider "aws" {
   region = "eu-west-1"
+}
+
+provider "tls" {
+  alias = "private_key"
 }

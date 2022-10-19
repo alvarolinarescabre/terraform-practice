7b6e0f72f6a1a029ee40c10cf94b616adf6d55eb
Alvaro "Chamo" Linares Cabre <alinares@paradigmadigital.com>
2022-10-19 19:29:50 +0200

    Update main.tf

--- Solution_EC2_Template/main.tf
+++ Solution_EC2_Template/main.tf
@@ -21,13 +21,13 @@ resource "aws_instance" "cice_instance" {
   key_name               = aws_key_pair.generated_key.key_name
 
   user_data = <<-EOF
-                #!/bin/bash
-                yum update
-                yum install -y httpd
-                service enable httpd
-                service start httpd
-                echo "<h1> Hello from Instance: $(hostname -f)</h1>" > /var/www/html/index.html
-              EOF
+              #!/bin/bash
+              yum update
+              yum install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              echo "<h1> Hello from Instance: $(hostname -f)</h1>" > /var/www/html/index.html
+            EOF
   tags = {
     Name = "CICE-Terraform"
   }

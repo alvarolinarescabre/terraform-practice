e3853e88abfdf7996124b596b574bc8cc4ae5510
Alvaro "Chamo" Linares Cabre <alvarolinarescabre@gmail.com>
2023-07-10 18:34:02 +0200

    Update sg.tf

--- Solution_RDS_Template/sg.tf
+++ Solution_RDS_Template/sg.tf
@@ -1,27 +1,27 @@
-resource "aws_security_group" "cice-sg" {
-  name        = "CICE Terraform Security Group"
-  description = "CICE Terraform Security Group"
+resource "aws_security_group" "sg" {
+  name        = "Terraform Security Group"
+  description = "Terraform Security Group"
 
   tags = {
-    Name = "CICE Terraform Security Group"
+    Name = "Terraform Security Group"
   }
 }
 
-resource "aws_security_group_rule" "cice-sg-mysql" {
+resource "aws_security_group_rule" "sg-mysql" {
   type              = "ingress"
   from_port         = 3306
   to_port           = 3306
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.cice-sg.id
+  security_group_id = aws_security_group.sg.id
 }
 
-resource "aws_security_group_rule" "cice-sg-egress" {
+resource "aws_security_group_rule" "sg-egress" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.cice-sg.id
+  security_group_id = aws_security_group.sg.id
 }
  

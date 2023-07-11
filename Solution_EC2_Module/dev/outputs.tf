55c93c2d2dc4dfc97cf08e59b443f029df06e100
Alvaro "Chamo" Linares Cabre <alvarolinarescabre@gmail.com>
2023-07-11 20:36:35 +0200

    Update outputs.tf

--- Solution_EC2_Module/dev/outputs.tf
+++ Solution_EC2_Module/dev/outputs.tf
@@ -1,14 +1,9 @@
-output "ec2_public_ip" {
-  description = "EC2 Public IP"
-  value       = module.cice_instance.ec2_public_ip
+output "webserver_public_ip" {
+  description = "Webserver Public IP"
+  value = module.webserver.ec2_public_ip
 }
 
-output "ec2_public_dns" {
-  description = "EC2 Public DNS"
-  value       = module.cice_instance.ec2_public_dns
-}
-
-output "ec2_tags" {
-  description = "EC2 Tags"
-  value       = module.cice_instance.ec2_tags
+output "webserver_key_pairs" {
+  description = "Webserver Key Pairs Private Name"
+  value = module.key_pairs.key_name
 }

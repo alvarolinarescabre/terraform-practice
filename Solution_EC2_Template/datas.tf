76155a83c4432633c0eb77ef24ba5ac936f8c5b9
Alvaro "Chamo" Linares Cabre <alvarolinarescabre@gmail.com>
2025-09-29 21:26:13 +0200

    Update datas.tf

--- Solution_EC2_Template/datas.tf
+++ Solution_EC2_Template/datas.tf
@@ -1,10 +1,24 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+data "aws_ami" "amzn_linux_2023_latest" {
+    most_recent = true
+    owners = [ "amazon" ]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+    filter {
+      name = "name"
+      values = [ "al2023-ami-2023*" ]
+    }
+
+    filter {
+      name = "architecture"
+      values = [ "x86_64" ]
+    }
+
+    filter {
+      name = "root-device-type"
+      values = [ "ebs" ]
+    }
+
+    filter {
+      name = "virtualization-type"
+      values = [ "hvm" ]
+    }
 }
-
